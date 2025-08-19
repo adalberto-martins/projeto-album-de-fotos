3ffc297042ef1132e3066caa01029a5a014a7d4b
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-08-19 08:42:39 -0300

    adicionando  JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+    function openImage(imgSrc) {
+        window.open(imgSrc, '_blank');
+    }
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+        const images = document.querySelectorAll('section.coluna img');
+        images.forEach((img, index) => {
+            // Adiciona um ID dinâmico a cada imagem
+            img.setAttribute('id', `img${index + 1}`);
+            
+            // Adiciona o evento de duplo clique
+            img.addEventListener('dblclick', function() {
+                openImage(this.src);
+            });
+        });
+    });
+</script>
